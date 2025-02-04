b70f84a9384c346df1dc1bd3721b9f4b4b3ea0b0
henrychris <64085210+henrychris@users.noreply.github.com>
2025-02-04 23:04:10 +0100

    use modules

--- script.js
+++ script.js
@@ -137,22 +137,7 @@ async function submitForm(event) {
     // Log the venue object (replace with API call later)
     console.log("Venue Data:", venue);
 
-    try {
-        // const { data, error } = await supabase
-        //     .from("venues")
-        //     .insert([venue])
-        //     .select();
-        // if (error) {
-        //     throw error;
-        // }
-        // console.log("Venue saved:", data);
-        // alert and reset form
-        // alert("Form submitted successfully!");
-        // form.reset();
-    } catch (error) {
-        console.error("Error saving venue:", error);
-        alert("Failed to submit form. Please try again.");
-    }
+    await saveToSupabase(venue);
 }
 
 function setupEventListeners() {
@@ -278,116 +263,9 @@ function setupEventListeners() {
             field.classList.remove("invalid-field");
         });
     });
-}
 
-function getMissingFields(fields) {
-    return Object.entries(fields)
-        .filter(([_, value]) => value === "")
-        .map(([key, _]) => ({
-            id: key.replace(" ", ""),
-            label: key.charAt(0).toUpperCase() + key.slice(1),
-        }));
-}
-
-function highlightInvalidFields(missingFields) {
-    // Remove existing highlights
-    document.querySelectorAll(".invalid-field").forEach((field) => {
-        field.classList.remove("invalid-field");
-    });
-
-    // Add highlights to missing fields
-    missingFields.forEach((field) => {
-        console.log("field", field);
-        const element = document.getElementById(field.id);
-        if (element) {
-            element.classList.add("invalid-field");
         }
     });
 }
 
-function alertMissingFields(missingFields) {
-    if (missingFields.length > 0) {
-        alert("Please fill in the required fields.");
-        highlightInvalidFields(missingFields);
-        return false;
-    }
-    return true;
-}
-
-// page validation functions
-// todo: can we get elements with `required` attribute from the DOM?
-function validatePage1() {
-    const fields = {
-        name: document.getElementById("name").value.trim(),
-        address: document.getElementById("address").value.trim(),
-        venueType: document.getElementById("venueType").value,
-        phoneNumber: document.getElementById("phoneNumber").value,
-        email: document.getElementById("email").value,
-    };
-
-    return alertMissingFields(getMissingFields(fields));
-}
-
-function validatePage2() {
-    const fields = {
-        pitchDimensions: document
-            .getElementById("pitchDimensions")
-            .value.trim(),
-        numberOfPitches: document.getElementById("numberOfPitches").value,
-    };
-
-    return alertMissingFields(getMissingFields(fields));
-}
-
-function validatePage3() {
-    const fields = {
-        venueHourlyRate: document.getElementById("venueHourlyRate").value, // todo: how are numbers handled
-    };
-
-    return alertMissingFields(getMissingFields(fields));
-}
-
-// todo: for numeric or array values, i can highlight the fields by passing a value to alertMissingFields().
-// input is of type - field: { id: string }
-function validatePage4() {
-    const fields = {
-        venueOpeningTime: document
-            .getElementById("venueOpeningTime")
-            .value.trim(), // todo: how is time handled
-        venueClosingTime: document
-            .getElementById("venueClosingTime")
-            .value.trim(), // todo: how is time handled
-        checkedDays: document.querySelectorAll('input[name="days"]:checked')
-            .length, // todo: how do we highlight this section?
-        venuePeakTimes: document.getElementById("venuePeakTimes").value.trim(),
-        averageVisitorsPerSession: document.getElementById(
-            "averageVisitorsPerSession"
-        ).value,
-    };
-
-    return alertMissingFields(getMissingFields(fields));
-}
-
-function validatePage5() {
-    return true;
-}
-function validatePage6() {
-    return true;
-}
-
-function validatePage7() {
-    const fields = {
-        surveyorName: document.getElementById("surveyorName").value.trim(),
-        surveyDate: document.getElementById("surveyDate").value.trim(), // todo: how are dates handled?
-    };
-
-    return alertMissingFields(getMissingFields(fields));
-}
-
 setupEventListeners();
-
-// Initialize Supabase
-const SUPABASE_URL = "https://udqlirsbugiflnrpfkdl.supabase.co";
-const SUPABASE_PUBLIC_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkcWxpcnNidWdpZmxucnBma2RsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNjg2NTcsImV4cCI6MjA1Mzg0NDY1N30.-LGuCvBEmV1w5YGTJb546wHW4wpQrJCHw4feiiur-HI";
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
